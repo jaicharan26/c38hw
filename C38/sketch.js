30d77f161af9f21f1c785d15c5a70c53ba36b338
jaicharan26 <68287266+jaicharan26@users.noreply.github.com>
2020-11-10 20:38:24 +0530

    Update sketch.js

--- C38/sketch.js
+++ C38/sketch.js
@@ -94,10 +94,9 @@ function draw() {
     score = score + Math.round(getFrameRate()/60);
     ground.velocityX = -(6 + 3*score/100);
     
-    if((touches.length > 0 || keyDown("SPACE")) && trex.y  >= height-120) {
+    if((keyDown("SPACE")) && trex.y  >= height-120) {
       jumpSound.play( )
       trex.velocityY = -10;
-       touches = [];
     }
     
     trex.velocityY = trex.velocityY + 0.8
